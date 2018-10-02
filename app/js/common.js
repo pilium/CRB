e4315fcb89f218d469fc6eea7177e96df9f31653
pilium <igorpilium@gmail.com>
2018-10-02 15:40:05 +0300

    add 3 sections
    add header-modal, news, quick-menu.
    Some fix/refactoring

--- app/js/common.js
+++ app/js/common.js
@@ -1,13 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
+    // const search = {
+    //     container: document.querySelector('.header-actions__search'),
+    //     trigger: document.querySelector('.icon-magnifier-tool'),
+    //     input: document.querySelector(".header-actions__search-input"),
+    //     showClass: 'active'
+    // }
+
+    // const modal = {
+    //     container: document.querySelector(".modal"),
+    //     trigger: document.querySelector(".js-header-info__writeToUs-link"),
+    //     input: document.querySelector(".modal-user__login-input"),
+    //     close: document.querySelector(".modal-close__btn"),
+    //     showClass: 'modal-show'
+    // }
+    // console.log(search);
+    // console.log(modal);
+
+    
     const container = document.querySelector('.header-actions__search');
     const search = container.querySelector('.icon-magnifier-tool');
     const input = container.querySelector("[type=text]");
 
     const link = document.querySelector(".js-header-info__writeToUs-link");
     const popup = document.querySelector(".modal");
-    const close = popup.querySelector(".modal-circle-btn");
+    const close = popup.querySelector(".modal-close__btn");
     const login = popup.querySelector("[name=login]");
+
+    // function toggle_class(trigger, container, input, showClass) {
+    //     trigger.addEventListener('click', function () {
+    //         container.classList.toggle(showClass);
+    //         if (input) {
+    //             input.focus();
+    //         }
+    //     })
+    //     document.addEventListener("keydown", function (event) {
+    //         if (event.keyCode == 27) {
+    //             if (container.classList.contains(showClass)) {
+    //                 container.classList.remove(showClass);
+    //             }
+    //         }
+    //     });
+    //     document.addEventListener('click', (e) => {
+    //         e.preventDefault()
+    //         let target = e.target;
+    //         let its_container = target == container || container.contains(target);
+    //         let search_is_active = container.classList.contains(showClass);
+    //         if (!its_container && search_is_active) {
+    //             container.classList.toggle(showClass);
+    //         }
+    //     })
+    // }
+
+    // toggle_class(search.trigger, search.container, search.input,search.showClass);
+    // toggle_class(modal.trigger, modal.container, modal.input,modal.showClass);
+
 
     let search_toggle = ()=> {
         search.addEventListener('click', function() {
@@ -44,9 +91,20 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
             popup.classList.remove("modal-show");
         });
-        
+
     }
 
-search_toggle();
-modal();
+    search_toggle();
+    modal();
+    
+    new Glide('.glide', {
+        type: 'slider',
+        autoplay: 8000,
+        keybord: true,
+        hoverpause: true,
+        rewind: true,
+        rewindDuration: 1000,
+        animationTimingFunc: 'cubic-bezier(0.665, 0.340, 0.740, 1.000)'
+    }).mount();
+
 });
