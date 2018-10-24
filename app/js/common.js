a4713db53d22f3c1e5a12bc82e02b6e8fad7d4eb
pilium <igorpilium@gmail.com>
2018-10-24 11:29:11 +0300

    add news , articles slider

--- app/js/common.js
+++ app/js/common.js
@@ -73,7 +73,20 @@ document.addEventListener("DOMContentLoaded", function () {
         speed: 1000,
         mousewheelControl: true
       })
-    
+      
+    var swiper = new Swiper('.blog-slider', {
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        mousewheel: {
+          invert: false,
+        },
+        // autoHeight: true,
+        pagination: {
+          el: '.blog-slider__pagination',
+          clickable: true,
+        }
+      });    
     if(mapContainer) {
         function init(){
         var myMap = new ymaps.Map("map", {
@@ -109,6 +122,5 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(addTimeFooter, 100);
     search_toggle();
     modal();
-    
-    
+
 });
